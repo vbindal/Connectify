9c75a3398634b4b95bc002524fbd69f9ced72998
Vishal Bindal <vbindal027@gmail.com>
2023-11-01 14:02:51 +0530

    added new files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import authReducer from "./state"
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import{
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistReducer
+}from "redux-persist"
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/es/integration/react";
+import { getDefaultNormalizer } from "@testing-library/react";
+const persistConfig =  {key : "root", storage, version : 1};
+const persistReducer = persistReducer(persistConfig,authReducer)
+const store = configureStore({
+  reducer : persistReducer,
+  middleware:(getDefaultMiddleware)=>{
+    getDefaultMiddleware:{
+      ignoredActions:[FLUSH,
+        REHYDRATE,
+        PAUSE,
+        PERSIST,
+        PURGE,
+        REGISTER]
+    }
+  }
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+      <App />
+      </PersistGate>
+    </Provider>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
